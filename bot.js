f1338d259539ce01ec462a9dd5c367963993c546
Colin Shevlin <cwshevlin@gmail.com>
2016-12-09 15:48:59 -0600

    Added a Rauner bot.

--- bot.js
+++ bot.js
@@ -5,37 +5,55 @@ var Twit = require('twit');
 var T = new Twit(require('./config.js'));
 
 // This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
+var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"};
+
+function openStream() {
+  var stream = T.stream('user', {track: '@raunerstemexpo'});
+
+  stream.on('tweet', function(tweet) {
+    var name = tweet.user.name;
+    var greetings = ["Hey", "Hello", "Bonjour", "Shouts out to", "Hola", "Sup", "Thanks for the hello,"];
+    var greeting = greetings[Math.floor(Math.random() * greetings.length)];
+
+    var status = greeting + " " + name + "!";
+    T.post('statuses/update', { status: status }, function(err, data, response) {
+      console.log(data);
+    });
+  });
+}
+
 
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
-	  // log out any errors and responses
-	  console.log(error, data);
-	  // If our search request to the server had no errors...
-	  if (!error) {
-	  	// ...then we grab the ID of the tweet we want to retweet...
-		var retweetId = data.statuses[0].id_str;
-		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
-			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-		})
-	  }
-	  // However, if our original search request had an error, we want to print it out here.
-	  else {
-	  	console.log('There was an error with your hashtag search:', error);
-	  }
-	});
+  T.get('search/tweets', mediaArtsSearch, function (error, data) {
+    // log out any errors and responses
+    // console.log(error, data);
+    // If our search request to the server had no errors...
+    if (!error) {
+      // ...then we grab the ID of the tweet we want to retweet...
+    var retweetId = data.statuses[0].id_str;
+    // ...and then we tell Twitter we want to retweet it!
+    T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+      if (response) {
+        console.log('Success! Check your bot, it should have retweeted something.', response);
+      }
+      // If there was an error with our Twitter call, we print it out here.
+      if (error) {
+        console.log('There was an error with Twitter:', error);
+      }
+    })
+    }
+    // However, if our original search request had an error, we want to print it out here.
+    else {
+      console.log('There was an error with your hashtag search:', error);
+    }
+  });
 }
 
 // Try to retweet something as soon as we run the program...
-retweetLatest();
+// retweetLatest();
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 60);
+// setInterval(retweetLatest, 1000 * 60 * 60);
+
+openStream();
